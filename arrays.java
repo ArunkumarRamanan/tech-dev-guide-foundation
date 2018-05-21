746434f01239e509e93913b701669f2cc8ca7351
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-05-22 07:57:06 +0800

    arrays: Basic arrays in Java

---
+++ arrays.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        // Initialization
+        int[] ages = {17, 21, 18, 19};
+        boolean[] student = new boolean[4];
+
+        student[0] = true;
+        student[1] = false;
+        student[2] = false;
+        student[3] = true;
+        
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
+        }
+     }
+}
